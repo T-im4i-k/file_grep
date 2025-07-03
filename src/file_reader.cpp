b0b7f53fbcbdb6b90c3f14dba64382a1f9caf435
Ivanov <ivanovtimofey1@gmail.com>
2025-07-03 19:30:34 +0200

    file_reader.cpp: Create file_reader.cpp File - Add definitions for FileReader class

---
+++ src/file_reader.cpp
@@ -0,0 +1,50 @@
+#include "file_reader.hpp"
+
+FileReader::FileReader(const std::string &file_name) : FileManip(file_name, {std::ios_base::in})
+{
+}
+
+void FileReader::seekg(const size_t pos)
+{
+    file_stream_.seekg(static_cast<long>(pos));
+    if (fail())
+    {
+        throw std::runtime_error("Failed To Set Read Position In File: " + file_name_);
+    }
+}
+
+[[nodiscard]] size_t FileReader::tellg()
+{
+    long pos = file_stream_.tellg();
+    if (fail() || pos < 0)
+    {
+        throw std::runtime_error("Failed To Get Read Position In File: " + file_name_);
+    }
+
+    return static_cast<size_t>(pos);
+}
+
+[[nodiscard]] std::optional<std::string> FileReader::readToString(const size_t string_size)
+{
+    std::string buff(string_size, '\0');
+    file_stream_.read(buff.data(), string_size);
+
+    if (fail() || file_stream_.gcount() != string_size)
+    {
+        return {};
+    }
+
+    return buff;
+}
+
+[[nodiscard]] std::optional<char> FileReader::readChar()
+{
+    char c = '\0';
+    file_stream_.get(c);
+
+    if (fail())
+    {
+        return {};
+    }
+    return c;
+}
