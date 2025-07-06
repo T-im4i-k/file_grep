f4349347c58db5188743dd7a1eb3e0f2250e721e
Ivanov <ivanovtimofey1@gmail.com>
2025-07-06 16:21:54 +0200

    file_manip.cpp: Modify file_manip.cpp File - Rename FileManip::fileName -> FileManip::filePath method - Rename file_name_ -> file_path_ attribute - Rename file_name -> file_path argument of FileManip constructor

--- src/file_manip.cpp
+++ src/file_manip.cpp
@@ -1,18 +1,18 @@
 #include "file_manip.hpp"
 
-FileManip::FileManip(const fs::path &file_name, OpenMods mods) : file_name_(file_name)
+FileManip::FileManip(const fs::path &file_path, OpenMods mods) : file_path_(file_path)
 {
     auto open_mode = std::ios_base::openmode(0);
     for (auto mode : mods)
     {
         open_mode |= mode;
     }
-    file_stream_.open(file_name, open_mode);
+    file_stream_.open(file_path, open_mode);
 
     if (fail())
     {
         throw std::ios_base::failure(
-            "Failed To Open The File: " + fileName().string());
+            "Failed To Open The File: " + filePath().string());
     }
 }
 
@@ -22,13 +22,13 @@ void FileManip::close()
     if (fail())
     {
         throw std::ios_base::failure(
-            "Failed To Close The File: " + fileName().string());
+            "Failed To Close The File: " + filePath().string());
     }
 }
 
-const fs::path &FileManip::fileName() const
+const fs::path &FileManip::filePath() const
 {
-    return file_name_;
+    return file_path_;
 }
 
 bool FileManip::fail() const
