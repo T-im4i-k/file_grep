291dd7cced4873188550873bca9bff9d1e62ee31
Ivanov <ivanovtimofey1@gmail.com>
2025-07-06 17:46:05 +0200

    dir_iterator.hpp: Create Wrapper Around recursive_directory_iterator - Add class declaration for DirIterator class - Class serves as a wrapper around fs::recursive_directory_iterator

---
+++ src/dir_iterator.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class DirIterator
+{
+public:
+    DirIterator(const fs::path &dir_path);
+
+    [[nodiscard]] const fs::directory_entry &curr() const;
+
+    void next();
+
+    [[nodiscard]] bool end() const;
+
+    [[nodiscard]] bool fail() const;
+
+    [[nodiscard]] fs::path dirPath() const;
+
+    void reset();
+
+private:
+    std::error_code ec_;
+    fs::recursive_directory_iterator dir_it_;
+    fs::path dir_path_;
+};
