983f795bf1ed653fb025e644900e7e4672d36f17
Ivanov <ivanovtimofey1@gmail.com>
2025-07-09 22:16:06 +0200

    roll_str.cpp: Add Definitions For RollStr Class

---
+++ src/roll_str.cpp
@@ -0,0 +1,70 @@
+#include "roll_str.hpp"
+
+#include <stdexcept>
+
+explicit RollStr::RollStr(std::string str) : str_(std::move(str)), begin_offset_(0)
+{
+}
+
+RollStr &RollStr::operator=(std::string str)
+{
+    str_ = std::move(str);
+    begin_offset_ = 0;
+
+    return *this;
+}
+
+explicit RollStr::operator std::string() const
+{
+    std::string buff(size(), '\0');
+
+    for (size_t i = 0; i < size(); ++i)
+    {
+        buff[i] = (*this)[i];
+    }
+
+    return buff;
+}
+
+void RollStr::roll_forward(const char new_char)
+{
+    if (empty())
+    {
+        return;
+    }
+
+    (*this)[0] = new_char;
+    begin_offset_ = (begin_offset_ + 1) % size();
+}
+
+[[nodiscard]] char &RollStr::operator[](const size_t idx)
+{
+    return const_cast<char &>((*this)[idx]);
+}
+
+[[nodiscard]] const char &RollStr::operator[](const size_t idx) const
+{
+    if (idx >= size())
+    {
+        throw std::out_of_range(
+            "RollStr::operator[]: Failed To Access Field Of RollStr. Index Is Out Of Range");
+    }
+
+    const size_t real_idx = (idx + begin_offset_) % size();
+    return str_[real_idx];
+}
+
+[[nodiscard]] size_t RollStr::size() const
+{
+    return str_.size();
+}
+
+[[nodiscard]] bool RollStr::empty() const
+{
+    return str_.empty();
+}
+
+std::ostream &operator<<(std::ostream &os, const RollStr &roll_str)
+{
+    return os << static_cast<std::string>(roll_str);
+}
