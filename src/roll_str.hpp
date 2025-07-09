1f4036dd3b3c78c285f161f7349711515e74850d
Ivanov <ivanovtimofey1@gmail.com>
2025-07-09 22:08:17 +0200

    roll_str.hpp: Create RollStr class - Class is a wrapper around std::string - RollStr::roll_forward moves discards first character and adds a new one to the end - Which allows for queue-like behaviour - Rolling is implemented using cyclic buffer

---
+++ src/roll_str.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+class RollStr
+{
+public:
+    explicit RollStr(std::string str);
+
+    RollStr &operator=(std::string str);
+
+    explicit operator std::string() const;
+
+    void roll_forward(const char new_char);
+
+    [[nodiscard]] char &operator[](const size_t idx);
+
+    [[nodiscard]] const char &operator[](const size_t idx) const;
+
+    [[nodiscard]] size_t size() const;
+
+    [[nodiscard]] bool empty() const;
+
+    friend std::ostream &operator<<(std::ostream &os, const RollStr &roll_str);
+
+private:
+    std::string str_;
+    size_t begin_offset_;
+};
