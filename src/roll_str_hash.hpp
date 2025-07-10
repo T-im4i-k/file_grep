d46ef4d5e6b96eb882f7d1a3b800a5008d474788
Ivanov <ivanovtimofey1@gmail.com>
2025-07-10 17:47:49 +0200

    roll_str_hash.hpp: Create RollStrHash Class - Class serves for computing hash of RollStr/std::string - RollStrHash::roll allows for effective recomputation of hash after rolling - Hash computation is implemented using polynomial hash

---
+++ src/roll_str_hash.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "roll_str.hpp"
+
+class RollStrHash
+{
+public:
+    static constexpr size_t DEFAULT_BASE = 257;
+    static constexpr size_t DEFAULT_MOD = 1'000'000'007;
+
+    explicit RollStrHash(size_t mod = DEFAULT_MOD, size_t base = DEFAULT_BASE);
+
+    void init(std::string str);
+
+    void init(RollStr roll_str);
+
+    void roll(const char new_char);
+
+    [[nodiscard]] size_t getHash() const;
+
+private:
+    RollStr roll_str_;
+
+    size_t mod_;
+    size_t base_;
+    size_t base_pow_ = 1;
+
+    size_t curr_hash_ = 0;
+};
